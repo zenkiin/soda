1678bc3806b13d2820b266466f2e5ee7ca0c76d4
Zenkin Maxim <120705488+zenkiin@users.noreply.github.com>
2024-06-02 14:38:27 +0300

    SQL for counters

---
+++ CountersInfo.sql
@@ -0,0 +1,5 @@
+INSERT INTO mysodaapp_counter (name, counter_id, token) VALUES 
+('kraeved.rounb.ru', 45794676, 'y0_AgAAAAAO1qdXAAvExAAAAAEEodilAACvgsAQ9MlND6whvD7ejwbHAaV-dg'),
+('rounb.ru', 42889784, 'y0_AgAAAAAO1qdXAAvExAAAAAEEodilAACvgsAQ9MlND6whvD7ejwbHAaV-dg'),
+('rznmemory.ru', 62336311, 'y0_AgAAAAAO1qdXAAvExAAAAAEEodilAACvgsAQ9MlND6whvD7ejwbHAaV-dg'),
+('rznoldenc.ru', 85243345, 'y0_AgAAAAAO1qdXAAvExAAAAAEEodilAACvgsAQ9MlND6whvD7ejwbHAaV-dg');
